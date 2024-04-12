32f47d82a2371e74ffd06a748b993253565099e9
Carlos González González <carlosglez.28695@gmail.com>
2024-04-12 13:38:10 +0200

    Added abilities tab styles

--- src/components/ability/ability.tsx
+++ src/components/ability/ability.tsx
@@ -14,7 +14,8 @@ function Ability(props: any) {
             const item = await fetch(data.ability.url).then((response) => response.json());
 			const enAbility = item.effect_entries.filter((entry: any) => entry.language.name == 'en')[0]
             
-            let ability = {name: data.ability.name, description: enAbility.effect};
+            let name = data.ability.name.charAt(0).toUpperCase() + data.ability.name.slice(1);
+            let ability = {name: name, description: enAbility.effect};
             setAbilityState(ability);
         } catch (error) {
             console.error('Error loading pokemon list:', error);

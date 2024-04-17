519c1610bb367c5aea75c8c639f33fcfa85253eb
Carlos González González <carlosglez.28695@gmail.com>
2024-04-17 20:42:31 +0200

    Added face and rotate animations

--- src/components/pokemon_detail/pokemon_detail.tsx
+++ src/components/pokemon_detail/pokemon_detail.tsx
@@ -40,33 +40,67 @@ function PokemonDetail(props: any) {
         <>
             {isVisible && (
                 <div className="modal-overlay" onClick={handleClose}>
-                    <div className='modal-sprite-container'>
-                        <div className='pokeball'>
-                            <div className='pokeball-button'></div>
-                            <PokemonPicture sprite={pokemon.sprites.front_default} name={pokemon.name} className="pokemon-sprite"/>
-                        </div> 
-                    </div>
-
-                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                        <button className="close-button" onClick={handleClose}>X</button>
-                        <div className="modal-pokemon-basic-info">
-                            <div className='modal-pokemon-name-container'>
-                                <span>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span>
-                            </div>
-                            <div className='modal-pokemon-types-container'>
-                                {types}
-                            </div>
+                    <div className='card-detail'>
+                        <div className='modal-sprite-container'>
+                            <div className='pokeball'>
+                                <div className='pokeball-button'></div>
+                                <PokemonPicture sprite={pokemon.sprites.front_default} name={pokemon.name} className="pokemon-sprite"/>
+                            </div> 
                         </div>
 
-                        <hr className="modal-separator"/>
+                        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                            <button className="close-button" onClick={handleClose}>X</button>
+                            <div className="modal-pokemon-basic-info">
+                                <div className='modal-pokemon-name-container'>
+                                    <span>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span>
+                                </div>
+                                <div className='modal-pokemon-types-container'>
+                                    {types}
+                                </div>
+                            </div>
 
-                        <Tabs tabs={tabs} />
+                            <hr className="modal-separator"/>
 
+                            <Tabs tabs={tabs} />
+
+                        </div>
                     </div>
                 </div>
             )}
         </>
     )
+
+    // return(
+    //     <>
+    //         {isVisible && (
+    //             <div className="modal-overlay" onClick={handleClose}>
+    //                 <div className='modal-sprite-container'>
+    //                     <div className='pokeball'>
+    //                         <div className='pokeball-button'></div>
+    //                         <PokemonPicture sprite={pokemon.sprites.front_default} name={pokemon.name} className="pokemon-sprite"/>
+    //                     </div> 
+    //                 </div>
+
+    //                 <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+    //                     <button className="close-button" onClick={handleClose}>X</button>
+    //                     <div className="modal-pokemon-basic-info">
+    //                         <div className='modal-pokemon-name-container'>
+    //                             <span>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span>
+    //                         </div>
+    //                         <div className='modal-pokemon-types-container'>
+    //                             {types}
+    //                         </div>
+    //                     </div>
+
+    //                     <hr className="modal-separator"/>
+
+    //                     <Tabs tabs={tabs} />
+
+    //                 </div>
+    //             </div>
+    //         )}
+    //     </>
+    // )
 }
 
 export default PokemonDetail;
